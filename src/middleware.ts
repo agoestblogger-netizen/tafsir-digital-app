0cecda044352e0c4fa106657e2c4fed6539f3e4e
Agus Han <agoest.blogger@gmail.com>
2026-03-12 10:47:22 +0700

    Edukasi Satpam & Revisi KTP

--- src/middleware.ts
+++ src/middleware.ts
@@ -15,6 +15,6 @@ export const config = {
      * - api (API routes, because API usually handles its own Auth/CORS)
      * Feel free to modify this pattern to include more paths.
      */
-    "/((?!_next/static|_next/image|favicon.ico|api|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)",
+    "/((?!_next/static|_next/image|favicon.ico|manifest.json|icon.png|api|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)",
   ],
 };
